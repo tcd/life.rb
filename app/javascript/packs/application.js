d54271ffe6c01af430f70bd54ca2ce405f2c6b43
Clay Dunston <dunstontc@gmail.com>
2020-09-14 18:19:09 -0500

    Add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,13 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "./stylesheets/application"
+
+import "jquery"
+import "popper.js"
+import "bootstrap"
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+})
