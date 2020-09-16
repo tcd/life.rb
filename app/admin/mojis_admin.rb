e13586e2521674c186f630cb4c5e27156ab31a2f
Clay Dunston <dunstontc@gmail.com>
2020-09-15 19:58:38 -0500

    Updates to `Moji` model & admin

--- app/admin/mojis_admin.rb
+++ app/admin/mojis_admin.rb
@@ -7,17 +7,45 @@ Trestle.resource(:mojis) do
   table do
     column(:emoji, align: :center)
     column(:name, truncate: false)
+    column(:category, align: :center)
+    column(:unicode_version, align: :center)
   end
 
   # # @param moji [Moji]
-  # form do |moji|
-  #   text_field :name
-  #
-  #   row do
-  #     col { datetime_field :updated_at }
-  #     col { datetime_field :created_at }
-  #   end
-  # end
+  form do |moji|
+
+    tab(:emoji) do
+      # if moji.new_record?
+      # else
+      #   static_field("Emoji", moji.emoji || "")
+      # end
+      text_field(:emoji)
+      text_field(:unicode_code_point)
+      divider()
+      # text_field(:name)
+      text_field(:unicode_name)
+      text_field(:personal_name)
+      divider()
+      text_field(:category)
+      divider()
+      text_field(:unicode_version)
+      text_field(:emoji_version)
+    end
+
+    tab(:metadata) do
+      render(JSONCodeBlock::Component.new(data: moji.as_json))
+    end
+
+    sidebar do
+      form_group(:name, label: false, class: "text-center") do
+        content_tag(:h1) { moji.name }
+      end
+      form_group(:emoji, label: false, class: "text-center") do
+        content_tag(:span, class: "big-emoji") { moji.emoji }
+      end
+    end
+
+  end
 
   # params do |params|
   #   params.require(:moji).permit(:name, ...)
--- app/models/moji.rb
+++ app/models/moji.rb
@@ -1,12 +1,17 @@
-# See
+# Emoji characters up to Unicode v13.0
+#
+# See:
 #
 # - [Unicode Full Emoji List](https://unicode.org/emoji/charts/full-emoji-list.html)
+#     - [Unicode Emoji Chart Format](https://unicode.org/emoji/format.html#col-name)
 # - [Unicode Emoji Data Files](https://unicode.org/Public/emoji/)
 # - [Emojipedia](https://emojipedia.org/)
 # - [GitHub Emoji Cheatsheet](https://gist.github.com/rxaviers/7360908)
 # - [About Unicode Emoji](https://blog.emojipedia.org/unicode-behind-the-curtain/)
 # - [github/gemoji](https://github.com/github/gemoji) - Emoji images and names.
 # - [ticky/ruby-emoji-regex](https://github.com/ticky/ruby-emoji-regex) - 💎 A pair of Ruby regular expressions for matching Unicode Emoji symbols.
+#
+# https://util.unicode.org/UnicodeJsps/character.jsp?a=1F916
 class Moji < ApplicationRecord
 
   # @return [String]
@@ -15,23 +20,73 @@ class Moji < ApplicationRecord
   # @return [String]
   NEWEST_GEMOJI_EMOJI_DATA  = "https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json".freeze()
 
+  # @return [Regexp]
+  UNICODE_DATA_PATTERN = /^(?<code_points>([[:xdigit:]]{4,5}\s)+)\s+; (?<status>\S+)\s+# (?<emoji>\S+)\s+E(?<emoji_version>\S+)\s(?<name>.*)$/.freeze()
+
+  # ============================================================================
+  # Callbacks
+  # ============================================================================
+
+  before_validation(:init)
+
+  # @return [void]
+  def init()
+    self.unicode_name&.downcase!
+    self.name&.downcase!
+    super()
+  end
+
   # ==========================================================================
   # Attributes
   # ==========================================================================
 
   # @!group Attributes
 
-  # @!attribute aliases
-  #   @return [Array<String>]
+  # @!attribute unicode_name
+  #   NOTE: This will be lowercase.
+  #
+  #   *CLDR* (*Common Locale Data Repository*) Short Name;
+  #   A unique, official identifier for a given Emoji (or unicode character)
+  #   @return [String]
+  validates("unicode_name", presence: true)
 
   # @!attribute emoji
   #   @return [String]
   validates("emoji", presence: true)
 
+  # @!attribute unicode_code_point
+  #   @return [String]
+  validates("unicode_code_point", presence: true)
+
+  # @!attribute unicode_code_points
+  #   @return [Array<String>]
+  attribute(:unicode_code_points, :string, array: true)
+
+  # @!attribute group
+  #   @return [String]
+
+  # @!attribute sub_group
+  #   @return [String]
+
+  # @!attribute unicode_version
+  #   @return [String]
+
   # @!attribute emoji_version
   #   @return [String]
 
-  # @!attribute group
+  # @!attribute category
+  #   Examples include:
+  #
+  #   - Smileys & Emotion
+  #   - People & Body
+  #   - Animals & Nature
+  #   - Food & Drink
+  #   - Travel & Places
+  #   - Activities
+  #   - Objects
+  #   - Symbols
+  #   - Flags
+  #
   #   @return [String]
 
   # @!attribute name
@@ -40,14 +95,13 @@ class Moji < ApplicationRecord
   # @!attribute personal_name
   #   @return [String]
 
-  # @!attribute sub_group
-  #   @return [String]
-
   # @!attribute tags
   #   @return [Array<String>]
+  attribute(:tags, :string, array: true)
 
-  # @!attribute unicode_version
-  #   @return [String]
+  # @!attribute aliases
+  #   @return [Array<String>]
+  attribute(:aliases, :string, array: true)
 
   # @!endgroup Attributes
 
